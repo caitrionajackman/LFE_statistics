09505acf6d5082d1cd16030e4f587756b138f60b
hannamag101 <hanna.adamski@yale.edu>
2024-11-06 13:07:51 -0800

    Global Phases Updated

--- Global_Local_Phases.py
+++ Global_Local_Phases.py
@@ -98,95 +98,28 @@ for element in all:
 lfe_phase_no_nan = lfe_phases.iloc[no_nans].reset_index()
 
 # Calculate & PLOT Global Phases for each Joined LFE Event
-def global_phase_figures(lfe_phase_no_nan, blood_splatter = False):
-    # Create two subsets of the data - 1) LFEs seen from Dawn (02 - 10 hr LST), and 2) LFEs seen from Dusk (14 - 22 hr LST)
-    LFE_df = lfe_phase_no_nan
-    dawn_lfe, = np.where((LFE_df['subLST'] >= 2) & (LFE_df['subLST'] <= 10 ))
-    dusk_lfe, = np.where((LFE_df['subLST'] >= 14) & (LFE_df['subLST'] <= 22))
+def global_phase_figures(lfe_phase_no_nan, dusk_dawn_plots = False):
+    if dusk_dawn_plots == True:
+        # Create two subsets of the data - 1) LFEs seen from Dawn (02 - 10 hr LST), and 2) LFEs seen from Dusk (14 - 22 hr LST)
+        LFE_df = lfe_phase_no_nan
+        dawn_lfe, = np.where((LFE_df['subLST'] >= 2) & (LFE_df['subLST'] <= 10 ))
+        dusk_lfe, = np.where((LFE_df['subLST'] >= 14) & (LFE_df['subLST'] <= 22))
 
-    dawn_phase = lfe_phase_no_nan.iloc[dawn_lfe].reset_index()
-    dusk_phase = lfe_phase_no_nan.iloc[dusk_lfe].reset_index()
+        dawn_phase = lfe_phase_no_nan.iloc[dawn_lfe].reset_index()
+        dusk_phase = lfe_phase_no_nan.iloc[dusk_lfe].reset_index()
 
-    # Generate GLOBAL Degree measurements 
-    north1 = np.array(dusk_phase["north phase"]) % 360
-    south1 = np.array(dusk_phase["south phase"]) % 360
-    north2 = np.array(dawn_phase["north phase"]) % 360
-    south2 = np.array(dawn_phase["south phase"]) % 360
+        # Generate GLOBAL Degree measurements 
+        north1 = np.array(dusk_phase["north phase"]) % 360
+        south1 = np.array(dusk_phase["south phase"]) % 360
+        north2 = np.array(dawn_phase["north phase"]) % 360
+        south2 = np.array(dawn_phase["south phase"]) % 360
 
-    dusk_phase['north_deg'] = north1
-    dusk_phase['south_deg'] = south1
-    dawn_phase['north_deg'] = north2
-    dawn_phase['south_deg'] = south2
+        dusk_phase['north_deg'] = north1
+        dusk_phase['south_deg'] = south1
+        dawn_phase['north_deg'] = north2
+        dawn_phase['south_deg'] = south2
 
-    if blood_splatter == True:
-        # DAWN Phases
-        fig, ax = plt.subplots(1, figsize = (14, 14))
-        ax.set_ylabel("North Phase ($^\circ$)", fontsize = 20)
-        ax.set_xlabel("South Phase ($^\circ$)", fontsize = 20)
-        ax.scatter(dawn_phase['south_deg'], dawn_phase['north_deg'], s= 5, color = 'red')
-
-        ax.axhline(90, linestyle = '--', color = 'black')
-        plt.axhline(180, linestyle = '--', color = 'black')
-        plt.axhline(270, linestyle = '--', color = 'black')
-        plt.axvline(90, linestyle = '--', color = 'black')
-        plt.axvline(180, linestyle = '--', color = 'black')
-        plt.axvline(270, linestyle = '--', color = 'black')
-        ax.tick_params(axis='both', which='major', pad=50, labelsize = 20)
-
-        positions =[(0,0), (180,0), (360,0), (270, 90), (0,180), (180,180), (360, 180), (90, 270), (0, 360), (180, 360), (360, 360)]
-        labels = ['South', 'Thin', 'South', 'In', 'Thick', 'North', 'Thick', 'Out', 'South', 'Thin', 'South']
-
-        for position, label in zip(positions, labels):
-            circle = Circle(position, 20, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1, clip_on = False)
-            ax.add_patch(circle)
-            ax.text(position[0], position[1], label, color = 'blue', fontsize = 20, va = 'center', ha = 'center', weight = 'bold')
-
-        plt.xlim(0, 361)
-        plt.xticks(np.arange(0, 361, 90))
-        plt.yticks(np.arange(0, 361, 90))
-        plt.ylim(0, 360)
-        plt.title(f"Dawn (02 - 10 hr LST) LFE Phase Distribution \n N = {np.shape(dawn_phase)[0]}", pad = 70, fontsize = 20)
-        #plt.savefig("/Users/hannamag/Desktop/LFE_statistics/figures/blue_red_distrib_lfes_short.jpeg", dpi = 300)
-        plt.show()
-
-        # Dusk Phases
-        fig, ax = plt.subplots(1, figsize = (14, 14))
-        ax.set_ylabel("North Phase ($^\circ$)", fontsize = 20)
-        ax.set_xlabel("South Phase ($^\circ$)", fontsize = 20)
-        ax.scatter(dusk_phase['south_deg'], dusk_phase['north_deg'], s= 5, color = 'red')
-
-        ax.axhline(90, linestyle = '--', color = 'black')
-        plt.axhline(180, linestyle = '--', color = 'black')
-        plt.axhline(270, linestyle = '--', color = 'black')
-        plt.axvline(90, linestyle = '--', color = 'black')
-        plt.axvline(180, linestyle = '--', color = 'black')
-        plt.axvline(270, linestyle = '--', color = 'black')
-        ax.tick_params(axis='both', which='major', pad=50, labelsize = 20)
-
-        positions =[(0,0), (180,0), (360,0), (270, 90), (0,180), (180,180), (360, 180), (90, 270), (0, 360), (180, 360), (360, 360)]
-        labels = ['South', 'Thin', 'South', 'In', 'Thick', 'North', 'Thick', 'Out', 'South', 'Thin', 'South']
-
-        for position, label in zip(positions, labels):
-            circle = Circle(position, 20, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1, clip_on = False)
-            ax.add_patch(circle)
-            ax.text(position[0], position[1], label, color = 'blue', fontsize = 20, va = 'center', ha = 'center', weight = 'bold')
-
-        #circle1 = Circle((0,0), 45, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1)
-        #circle2 = Circle((0,0), 45, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1)
-        #circle3 = Circle((0,0), 45, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1)
-        #circle1 = Circle((0,0), 45, facecolor = 'none', edgecolor='blue', linewidth=3, alpha=1)
-
-        #ax.add_patch(circle1)
-
-        plt.xlim(0, 361)
-        plt.xticks(np.arange(0, 361, 90))
-        plt.yticks(np.arange(0, 361, 90))
-        plt.ylim(0, 360)
-        plt.title(f"Dusk (14 - 22 hr LST) LFE Phase Distribution \n N = {np.shape(dusk_phase)[0]}", pad = 70, fontsize = 20)
-        #plt.savefig("/Users/hannamag/Desktop/LFE_statistics/figures/blue_red_distrib_lfes_short.jpeg", dpi = 300)
-        plt.show()
-
-    else: # Plot Dawn & Dusk Contours
+        # Plot Dawn & Dusk Contours
         # DUSK - CONTOURS
         dusk_phase['vals1'] = np.ones(np.shape(dusk_phase)[0])
         dusk_phase['north1'] = np.ones(np.shape(dusk_phase)[0])
@@ -331,6 +264,84 @@ def global_phase_figures(lfe_phase_no_nan, blood_splatter = False):
         #plt.savefig("Countours_Dawn.jpeg", dpi = 300)
         plt.show()
 
+    else: # Global phases across Dusk & Dawn
+        north = np.array(lfe_phase_no_nan["north phase"]) % 360
+        south = np.array(lfe_phase_no_nan["south phase"]) % 360
+
+        lfe_phase_no_nan['north_deg'] = north
+        lfe_phase_no_nan['south_deg'] = south
+
+        joint_phases_wout_nan = lfe_phase_no_nan # Variable name change
+
+        joint_phases_wout_nan['vals1'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+        joint_phases_wout_nan['north1'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+        joint_phases_wout_nan['south1'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+        joint_phases_wout_nan['vals2'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+        joint_phases_wout_nan['north2'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+        joint_phases_wout_nan['south2'] = np.ones(np.shape(joint_phases_wout_nan)[0])
+
+        x1 = np.arange(-15, 405, 30) # 360
+        y1 =np.arange(-15, 405, 30) #360
+        
+        x2 = np.arange(-15, 375, 30)
+        y2 = np.arange(-15, 375, 30)
+
+        for i in range(np.shape(joint_phases_wout_nan)[0]):
+            joint_phases_wout_nan['north1'].iloc[i] = x1[np.where(joint_phases_wout_nan['north_deg'].iloc[i] - x1 >= 0)[0][-1]]
+            joint_phases_wout_nan['south1'].iloc[i] = x1[np.where(joint_phases_wout_nan['south_deg'].iloc[i] - x1 >= 0)[0][-1]]
+            joint_phases_wout_nan['north2'].iloc[i] = x2[np.where(joint_phases_wout_nan['north_deg'].iloc[i] - x2 >= 0)[0][-1]]
+            joint_phases_wout_nan['south2'].iloc[i] = x2[np.where(joint_phases_wout_nan['south_deg'].iloc[i] - x2 >= 0)[0][-1]]
+
+        Z1 = pd.pivot_table(joint_phases_wout_nan, index = 'north1', columns = 'south1', values = 'vals1', aggfunc = sum)
+        Z2 = pd.pivot_table(joint_phases_wout_nan, index = 'north2', columns = 'south2', values = 'vals2', aggfunc = sum)
+
+        X1, Y1 = np.meshgrid(x1,y1)
+        X2, Y2 = np.meshgrid(x2,y2)
+        Z1['360.0'] = np.zeros(13)
+
+        # New row with different column values
+        new_row = {Z1.columns[0]: 0, Z1.columns[1]: 0 , Z1.columns[2]: 0, Z1.columns[3]: 0, Z1.columns[4]: 0, Z1.columns[5]: 0 , 
+                Z1.columns[6]: 0, Z1.columns[7]: 0, Z1.columns[8]: 0, Z1.columns[9]: 0 , Z1.columns[10]: 0, Z1.columns[11]: 0, Z1.columns[12]: 0, Z1.columns[13]: 0}
+
+        # Convert the new row to a DataFrame
+        new_row_df = pd.DataFrame([new_row])
+
+        # Append the new row to the DataFrame
+        df = pd.concat([Z1, new_row_df])                
+        index_list = df.index.tolist()
+        index_list[-1] = 360
+
+        df.index = index_list
+        Z1 = df
+
+        # 2d histogram - both contours are [0,15, ..., 345]
+        fig, ax = plt.subplots(figsize=(10,10))
+        ax.set_ylabel("North Phase ($^\circ$)", fontsize = 20)
+        ax.set_xlabel("South Phase ($^\circ$)", fontsize = 20)
+        ax.set_title(f"Total LFE Phase Distribution \n N = {np.shape(joint_phases_wout_nan)[0]}", fontsize = 20)
+
+        plot1 = ax.contour(X1, Y1, Z1, levels = 15, cmap = 'spring_r')
+        Z_rev = np.matrix(Z2)[::-1]
+        plot2 = ax.imshow(Z_rev, cmap='Greys_r', extent=[-15, 375, -15, 375]) 
+
+        ax.set_xticks(np.arange(-15, 375, 30))
+        ax.set_xticklabels(np.arange(-15, 375, 30), rotation = 90)
+        ax.set_yticks(np.arange(-15, 375, 30))
+
+        ax.set_xlim(0, 360)
+        ax.set_ylim(0, 360)
+
+        cbar = fig.colorbar(plot2, ax = ax, shrink = 0.74)
+        cbar.add_lines(plot1)
+        cbar.ax.get_yaxis().labelpad = 15
+        cbar.ax.set_ylabel('# of LFE occurrences', rotation=270)
+        cbar.ax.tick_params(labelsize = 12)
+
+        fig.tight_layout()
+        plt.show() # ----- EX. /Sample_Visualizations/Contours_Global_Joined_LFEs.jpeg
+
+
+
 def local_phase_figures(lfe_phase_no_nan, trajectories_df):
     # Calculate Global Phases (degrees) for ALL JOINT LFEs
     north = np.array(lfe_phase_no_nan["north phase"]) % 360
@@ -436,5 +447,5 @@ def local_phase_figures(lfe_phase_no_nan, trajectories_df):
     plt.show()
 
 # Actual Code 
-global_phase_figures(lfe_phase_no_nan) # ----- EX. /Sample_Visualizations/Contours_Global_Joined_LFEs.jpeg
-local_phase_figures(lfe_phase_no_nan) # ----- EX. /Sample_Visualizations/Contours_Local_Joined_LFEs.jpeg
+global_phase_figures(lfe_phase_no_nan, dusk_dawn_plots=False) # ----- EX. /Sample_Visualizations/Contours_Global_Joined_LFEs.jpeg
+local_phase_figures(lfe_phase_no_nan, trajectories_df=trajectories_df) # ----- EX. /Sample_Visualizations/Contours_Local_Joined_LFEs.jpeg
